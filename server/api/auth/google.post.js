6c85fe57cf93c6745f34658547bf766f9fb6bf4e
Occur55170 <tw17055@gmail.com>
2024-11-29 01:08:11 +0800

    google data ex

--- server/api/auth/google.post.js
+++ server/api/auth/google.post.js
@@ -1,5 +1,37 @@
 import { OAuth2Client } from 'google-auth-library'
 
 export default defineEventHandler(async (event) => {
-  return 'aaa'
+  const body = await readBody(event)
+  const oauth2Client = new OAuth2Client({
+    clientId: '你的 Google Client ID',
+    clientSecret: '你的 Google Client Secret',
+    redirectUri: '你的 Google Redirect Uri'
+  })
+
+  let { tokens } = await oauth2Client.getToken(body.authCode)
+  oauth2Client.setCredentials({ access_token: tokens.access_token })
+
+  const userInfo = await oauth2Client
+    .request({
+      url: 'https://www.googleapis.com/oauth2/v3/userinfo'
+    })
+    .then((response) => response.data)
+    .catch(() => null)
+
+  oauth2Client.revokeCredentials()
+
+  if (!userInfo) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Invalid token'
+    })
+  }
+
+  return {
+    id: userInfo.sub,
+    name: userInfo.name,
+    avatar: userInfo.picture,
+    email: userInfo.email,
+    emailVerified: userInfo.email_verified,
+  }
 })
