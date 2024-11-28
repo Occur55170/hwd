891a2060d7f2524bbb2b93ac7fcdf92e3c3d5056
Occur55170 <tw17055@gmail.com>
2024-11-29 01:05:37 +0800

    remove google id

--- server/api/auth/google.post.js
+++ server/api/auth/google.post.js
@@ -2,11 +2,11 @@ import { OAuth2Client } from 'google-auth-library'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-  const oauth2Client = new OAuth2Client({
-    clientId: '你的 Google Client ID',
-    clientSecret: '你的 Google Client Secret',
-    redirectUri: '你的 Google Redirect Uri'
-  })
+  // const oauth2Client = new OAuth2Client({
+  //   clientId: '你的 Google Client ID',
+  //   clientSecret: '你的 Google Client Secret',
+  //   redirectUri: '你的 Google Redirect Uri'
+  // })
 
   let { tokens } = await oauth2Client.getToken(body.authCode)
   oauth2Client.setCredentials({ access_token: tokens.access_token })
